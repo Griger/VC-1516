9fc20b07affedb4b2716d25570692ef980c7fa8e
Griger <gustavo.rg92@gmail.com>
2015-12-18 00:11:00 +0100

    Empezando a trabajar un poquito.

---
+++ Trabajo3/src/main.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+#include <stdlib.h>
+
+using namespace std;
+
+/*
+Funcion que devuelve un valor aleatorio en el intervalo [0,1]
+*/
+float generadorAleatorio() {
+	return static_cast <float> (rand()) / static_cast <float> (RAND_MAX);
+}
+
+/*
+Funcion que genera una camara aleatoria 
+*/
+Mat generarCamaraAleatoria() {
+	camara = Mat(3,4,CV_32F);
+	camara_correcta = false;
+
+	//Mientras que la camara no tenga el menor distinto de cero
+	while (!camara_correcta) {
+		for (int i = 0; i < 3; i++)
+			for (int j = 0; j < 4; j++)
+				camara.at<float>(i,j) = generadorAleatorio();
+
+		//Tomamos la submatriz a la que queremos calcular el determinante
+		Mat submatriz = Mat(camara, Range::all(), Range(0,3));
+
+		if (determinant(submatriz) != 0)
+			camara_correcta = true;
+	}
+
+	return camara;
+}
+
+//Generacion de puntos del mundo:
+vector<Point3f> generarPtosMundo() {
+	vector<Point3f> puntos_mundo;
+	for (int i = 1; i <= 10;  i++)
+		for (int j = 1; j <= 10; j++){
+			puntos_mundo.push_back(Point3f(0,i*0.1, j*0.1));
+			puntos_mundo.push_back(Point3f(j*0.1, i*0.1, 0));
+		}
+
+	return puntos_mundo;
+}
+
+//Funcion donde se estructura los pasos necesarios para el primer punto de la practica
+void parte1() {
+	camara_generada = generarCamaraAleatoria();
+	ptos_mundo = generarPtosMundo();
+
+
+}
+
+
+int main() {
+	parte1();
+}
